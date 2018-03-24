e846920e7ba8d17d9446696086e9fbd0a3303eb6
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-24 09:57:26 -0500

    Created HTML header, game instructions, start button, main and footer. Created CSS styles for header, main and footer.

---
+++ jsracer/js/main.js
@@ -0,0 +1,7 @@
+console.log("Ready to Play");
+
+// Create background
+// Create racers
+// Create finish line
+// Move racer right on button click (right arrow & lowercase d)
+// Declare winner when racer crosses finish line
