c0303b680993aa80b11bff395e1bfd11de18a9b5
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-24 12:16:42 -0500

    Commented out HTML header to focus on moving cars and creating finish line, taking a break for lunch

--- jsracer/js/main.js
+++ jsracer/js/main.js
@@ -5,3 +5,8 @@ console.log("Ready to Play");
 // Create finish line
 // Move racer right on button click (right arrow & lowercase d)
 // Declare winner when racer crosses finish line
+
+document.getElementById("startRace").addEventListener("click", function(){
+    console.log("Button Clicked");
+    document.getElementById("upperCar").style.marginLeft = "50px";
+});
