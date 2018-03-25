8837739297cf2f2579a82b041b8d41130edafeb2
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-24 20:51:19 -0500

    Got Start Race button working, updates HTML to show GO, working on delay to see coundown on HTML page. Put Start New Race button and code to reset cars to starting point and add points to correct player

--- jsracer/js/main.js
+++ jsracer/js/main.js
@@ -13,13 +13,38 @@ let redLeft = 0;
 let purpleLeft = 0;
 let redWin = 0;
 let purpleWin = 0;
+let p1Total = 0;
+let p2Total = 0;
+let countDown = 4;
+
+const resetRace = () => {
+  redLeft = 0;
+  purpleLeft = 0;
+  countDown = 4;
+  redcar.style.left = redLeft + "px";
+  purplecar.style.left = purpleLeft + "px";
+  document.getElementById('timer').innerHTML = "3";
+}
+
+const letsRace = () => {
+  console.log("Start the countdown");
+  for (let i=3; i>=0; i--) {
+    countDown -= 1;
+    console.log(countDown);
+    document.getElementById('timer').innerHTML = "Go!";
+  }
+}
 
 const checkWinner = () => {
-  if (redLeft >=720) {
+  if (redLeft >=750) {
     alert("Player 1 Wins");
+    p1Total += 1;
+    document.getElementById('p1score').innerHTML = p1Total;
   } else {
-    if (purpleLeft >= 720) {
+    if (purpleLeft >= 750) {
       alert("Player 2 Wins");
+      p2Total += 1;
+      document.getElementById('p2score').innerHTML = p2Total;
     }
   }
 }
@@ -30,17 +55,11 @@ const anim = (event) => {
     redLeft += 40;
     redcar.style.left = redLeft + "px";
     checkWinner();
-    // if (redLeft >= 720) {
-    //   alert("Red Win!");
-    // }
   }
   if (event.keyCode == 68) {
     purpleLeft += 40;
     purplecar.style.left = purpleLeft + "px";
     checkWinner();
-    // if (purpleLeft >= 720) {
-    //   alert("Purple Wins!");
-    // }
   }
 }
 
