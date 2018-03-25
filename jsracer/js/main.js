d93525529251a930b782eaa559f46e350a9a73b4
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-25 10:29:00 -0500

    Created countdown timer to start race

--- jsracer/js/main.js
+++ jsracer/js/main.js
@@ -15,26 +15,27 @@ let redWin = 0;
 let purpleWin = 0;
 let p1Total = 0;
 let p2Total = 0;
-let countDown = 4;
+// let countDown = 4;
 
 // reset cars positions and countdown starting point
 const resetRace = () => {
   redLeft = 0;
   purpleLeft = 0;
-  countDown = 4;
+  // countDown = 4;
   redcar.style.left = redLeft + "px";
   purplecar.style.left = purpleLeft + "px";
-  document.getElementById('timer').innerHTML = "3";
+  document.getElementById('timer').innerHTML = "5";
 }
 
-// start the race button, countdown 3, 2, 1, GO
+// start the race button, countdown 5, 4, 3, 2, 1, 0
 const letsRace = () => {
-  console.log("Start the countdown");
-  for (let i=3; i>=0; i--) {
-    countDown -= 1;
-    console.log(countDown);
-    document.getElementById('timer').innerHTML = "Go!";
-  }
+  let countDown = 5;
+  let countDownTimer = setInterval(function() {
+    countDown--;
+    document.getElementById('timer').innerHTML = countDown;
+    if (countDown <=0)
+      clearInterval(countDownTimer);
+  }, 1000);
 }
 
 // check winner of race, first player to reach end of
@@ -58,13 +59,13 @@ const checkWinner = () => {
 // check for winner
 const move = (event) => {
   if (event.keyCode == 39) {
-    console.log("red car moved");
+    // console.log("red car moved");
     redLeft += 40;
     redcar.style.left = redLeft + "px";
     checkWinner();
   }
   if (event.keyCode == 68) {
-    console.log("purple car moved");
+    // console.log("purple car moved");
     purpleLeft += 40;
     purplecar.style.left = purpleLeft + "px";
     checkWinner();
