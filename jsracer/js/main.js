4faf72f80c6dffc223d2801913d0ab72ede05258
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-24 19:20:52 -0500

    got cars to move and alert displays to show which car won

--- jsracer/js/main.js
+++ jsracer/js/main.js
@@ -1,12 +1,35 @@
 console.log("Ready to Play");
 
-// Create background
-// Create racers
-// Create finish line
-// Move racer right on button click (right arrow & lowercase d)
-// Declare winner when racer crosses finish line
+// move player 1 using d key
+// listen for d keyup action
 
-document.getElementById("startRace").addEventListener("click", function(){
-    console.log("Button Clicked");
-    document.getElementById("upperCar").style.marginLeft = "50px";
-});
+// move player 2 using right arrow key
+// alert winner when player reaches end of div
+
+let redcar = document.getElementById('redcar');
+let purpleCar = document.getElementById('purplecar')
+
+let redLeft = 0;
+let purpleLeft = 0;
+let redWin = 0;
+let purpleWin = 0;
+
+const anim = (event) => {
+  if (event.keyCode == 39) {
+    console.log("red car moved");
+    redLeft += 40;
+    redcar.style.left = redLeft + "px";
+    if (redLeft >= 720) {
+      alert("Red Win!");
+    }
+  }
+  if (event.keyCode == 68) {
+    purpleLeft += 40;
+    purplecar.style.left = purpleLeft + "px";
+    if (purpleLeft >= 720) {
+      alert("Purple Wins!");
+    }
+  }
+}
+
+document.onkeyup = anim;
